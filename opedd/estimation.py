28370d5733a0b4ecd1bf31c7f2c6d48b3adf093d
Will-Consagra <wxc5756@rit.edu>
2022-08-31 13:20:54 -0700

    bug fix

--- opedd/estimation.py
+++ opedd/estimation.py
@@ -27,11 +27,11 @@ def roughness_penalized_ridge_estimator(X, S, sh_order, gamma):
 		sh_order: int, order of real, symmetric spherical harmonic basis: k = int((sh_order+1)*(sh_order+2)/2)
 		gamma: float, regularization strength >= 0
 	"""
-	X_spherical = cart2sphere(X_i)
+	X_spherical = cart2sphere(X)
 	theta = X_spherical[:,0]; phi = X_spherical[:,1]
 	B, m, n = real_sym_sh_basis(sh_order, phi, theta) # B N_i x K ... m == power, length K array , n == order, length K array 
 	R = np.diag(np.power(n, 2)*np.power(n+1, 2))
-	c_hat = np.dot(np.linalg.inv(np.dot(B.T, B) + gamma*R) , np.dot(B.T, S_i))
+	c_hat = np.dot(np.linalg.inv(np.dot(B.T, B) + gamma*R) , np.dot(B.T, S))
 	P = np.dot(np.dot(B, np.linalg.inv(np.dot(B.T, B) + gamma*R)), B.T)
 	return c_hat, P
 
