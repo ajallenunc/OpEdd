d6e45046aeb51d297961ac5c05d9d45e0248b028
Adrian Allen <aallen1@c1416ood03.ll.unc.edu>
2023-08-03 09:47:21 -0400

    Changed slurm queueing logic and folder expectations

--- priors/construct_prior.sh
+++ priors/construct_prior.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 #
-
-#### replace 'myCondEnv3' with your anaconda3 created python environment
-conda activate myCondEnv3
+#### replace 'OpTryEnv' with your anaconda3 created python environment
+source ~/.bashrc_opedd
+conda activate OpTryEnv
 
 module load ants
-
-# CHANGE FOR SPECIFIC SBATCH OPTIONS
-OPTIONS="-p dmi --qos abcd"
 
 # exit when any command fails
 set -e
@@ -20,7 +17,7 @@ source config.txt
 
 TRAIN_IDS=${OpEddFolder}/training_ids.txt
 PRIOR_DIR=${DaTaFolder}/priors/${PRIOR_NAME}
-DATA_DIR=${DaTaFolder}/DWI_unregistered
+DATA_DIR=${DaTaFolder}/priors/${PRIOR_NAME}/DWI_unregistered
 
 # create a unique job name prefix
 JID=$(uuidgen | tr '-' ' ' | awk {'print $1}')
@@ -29,18 +26,24 @@ JID=$(uuidgen | tr '-' ' ' | awk {'print $1}')
 mapfile -t subjects < $TRAIN_IDS
 
 ## Build Prior 
-# Step 1: fit diffuison signal
+declare -a job_ids_step2
 for i in $(seq 1 ${#subjects[@]}); do
         idx=$((i - 1))
-        sbatch fit_historical_signal.sh $DATA_DIR $PRIOR_DIR ${subjects[$idx]}
+
+        # Create training directory and copy over files
+        mkdir -p $PRIOR_DIR/training/${subjects[$idx]}
         cp $DATA_DIR/${subjects[$idx]}/geom_field.nii.gz $PRIOR_DIR/training/${subjects[$idx]}
+
+        # Step 1: Fit diffusion signal
+        job_id_step1=$(sbatch fit_historical_signal.sh $DATA_DIR $PRIOR_DIR ${subjects[$idx]} | awk '{print $NF}')
+
+        # Step 2: Register diffusion signal to template
+        job_id_step2=$(sbatch --dependency=afterok:$job_id_step1 warp_diffusion_signal.sh $DATA_DIR $PRIOR_DIR ${subjects[$idx]} | awk '{print $NF}')
+
 done
-wait 
-# Step 2: Register diffusion signal to template 
-for i in $(seq 1 ${#subjects[@]}); do
-    idx=$((i - 1))
-    sbatch warp_diffusion_signal.sh $DATA_DIR $PRIOR_DIR ${subjects[$idx]}
-done
-wait
-# Step 3: Estimate mean and covariance functions on the template space 
-sbatch estimate_mean_cov.sh $PRIOR_DIR
+
+# Combine job IDs into a comma-separated list for dependency
+job_dependency_step2=$(IFS=, ; echo "${job_ids_step2[*]}")
+
+# Step 3: Estimate mean and covariance functions on the template space
+sbatch --dependency=afterok:$job_dependency_step2 estimate_mean_cov_2.sh $PRIOR_DIR
