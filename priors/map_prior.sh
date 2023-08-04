6e55d57d68e4be9f988f835aed478342dbab7cbd
Adrian Allen <aallen1@longleaf-login4.its.unc.edu>
2023-08-04 08:29:55 -0400

    Added code to estimate fODF for varying budgets

--- priors/map_prior.sh
+++ priors/map_prior.sh
@@ -2,12 +2,9 @@
 #
 
 #### replace 'myCondEnv3' with your anaconda3 created python environment
-conda activate myCondEnv3
+conda activate OptTryEnv
 
 module load ants
-
-# CHANGE FOR SPECIFIC SBATCH OPTIONS
-OPTIONS="-p dmi --qos abcd"
 
 # exit when any command fails
 set -e
@@ -18,14 +15,25 @@ trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 # get global config 
 source config.txt
 
+PRIOR_NAME=${1}
 PRIOR_DIR=${DaTaFolder}/priors/${PRIOR_NAME}
+test_ids=${PRIOR_DIR}/testing_ids.txt
+budget_list="5 10 20 30 40 50"
 
-# Step 4: Map mean and covariance functions into the subject space 
-sbatch warp_prior.sh $PRIOR_DIR $SUBJECT_DIR
+while IFS= read -r sub
+do
+    echo $sub
+    # Step 1: Map Prior to Subject Space
+    job1=$(sbatch warp_prior.sh $PRIOR_DIR "$PRIOR_DIR"/testing/$sub | cut -f 4 -d' ')
 
-# Step 5: Create EB object for each training subject
-sbatch compute_EB.sh $SUBJECT_DIR
+    # Step 2: Create EB object for each test subject
+    job2=$(sbatch --dependency=afterok:$job1 compute_EB.sh "$PRIOR_DIR"/testing/$sub | cut -f 4 -d' ')
 
-# Step 6 (optional) Run example analysis 
+    # Step 3: Estimate fODF
+    for b in $budget_list
+    do
+        sbatch --mem=200G --time=2- --job-name=sub_${sub}_${b}_opedd_fodf_job --output=out_slurm/opedd_signal_to_fodf_${sub}_${b}.out --error=out_slurm/opedd_signal_to_fodf_${sub}_${b}.err --dependency=afterok:$job2 --wrap="python -u opedd_signal_to_fodf.py --subject_dir=\"$PRIOR_NAME\"/testing/$sub --Bval=2000 --M=$b --sh_order=8"
 
-##python example.py --subject_dir $SUBJECT_DIR --sh_order 8 --Bval 2000 --n_voxel_samps 100 --M
+    done
+done < "$test_ids"
+
